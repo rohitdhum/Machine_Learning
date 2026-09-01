01373009488df8d2369315d09318a1d9794f4c7a
rohitdhum <rohit45dhumal@gmail.com>
2026-09-01 14:04:42 +0530

    Add files via upload

---
+++ Individual_BreastCancer_DT.py
@@ -0,0 +1,73 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+#---------------------------------------
+# Step 1 : load the Dataset
+#---------------------------------------
+
+df = pd.read_csv("breast_cancer.csv")
+
+print("Shape of Datset :", df.shape)
+
+print("First few records :")
+print(df.head())
+
+#---------------------------------------
+# Step 2 : Separate features and labels
+#---------------------------------------
+
+X = df.drop("target", axis=1)
+Y = df["target"]
+
+print("X shape :", X.shape)
+print("Y shape :", Y.shape)
+
+#---------------------------------------
+# Step 3 : Split Dataset for traing and testing
+#---------------------------------------
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,
+                                                   Y,
+                                                   test_size=0.2,
+                                                   random_state=42
+                                                   )
+
+#---------------------------------------
+# Step 4 : Scale the features
+#---------------------------------------
+
+scalar = StandardScaler()
+
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
+
+#---------------------------------------
+# Step 5 : Create the model
+#---------------------------------------
+
+model = DecisionTreeClassifier(random_state=42)
+
+#---------------------------------------
+# Step 6 : Train the model
+#---------------------------------------
+
+model = model.fit(X_train, Y_train)
+
+#---------------------------------------
+# Step 7 : Test the model
+#---------------------------------------
+
+Y_pred = model.predict(X_test)
+
+#---------------------------------------
+# Step 8 : Evaluate the model
+#---------------------------------------
+
+print("Accuracy :", accuracy_score(Y_test, Y_pred))
+
+print("Confusion Matrix :")
+print(confusion_matrix(Y_test, Y_pred))
