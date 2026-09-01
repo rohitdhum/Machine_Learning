03a99bad2067f13db72b293f1ea561d66cf6de14
rohitdhum <rohit45dhumal@gmail.com>
2026-09-01 14:07:01 +0530

    Add files via upload

---
+++ LinearRegressionAdvertising6.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import mean_absolute_error, r2_score 
+
+def MarvellousRegression(DataPath):
+    border = "_"* 40
+
+    # Step 1 : Load the data
+    print(border)
+    print("Step 1 : Load the Data")
+    print(border)
+
+    df = pd.read_csv(DataPath)
+
+    print(df.head())
+
+    # Step 2 : Remove unwanted columns
+    print(border)
+    print("Step 2 : Remove unwanted columns")
+    print(border)
+
+    if "Unnamed: 0" in df.columns:
+        df = df.drop(columns=["Unnamed: 0"])
+
+    print(df.head())
+
+    # Step 3 : Check Missing Values
+    print(border)
+    print("Step 3 : Check Missing Values")
+    print(border) 
+
+    print("Total Missing Values :")
+    print(border)
+    print(df.isnull().sum())
+    print(border)
+
+    # Step 4 : Statistical Summary
+    print(border)
+    print("Step 4 : Statistical Summary")
+    print(border) 
+
+    print(df.describe())
+
+    # Step 5 : Correlation 
+    print(border) 
+    print("Step 5 : Correlation") 
+    print(border) 
+
+    print(df.corr()) 
+
+    # Step 6 : Separate Independent and Dependent Variables
+    print(border)
+    print("Step 6 : Separate Independent and Dependent Variables")
+    print(border) 
+
+    X = df[["TV", "radio", "newspaper"]]
+    Y = df["sales"]
+
+    print("Indepedent Variables :")
+    print(X.head())
+
+    print("Depedent Variables :")
+    print(Y.head())
+
+    # Step 7 : Split the Dataset
+    print(border)
+    print("Step 7 : Split the Dataset")
+    print(border)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X,
+        Y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    print("Traning Data :", X_train.shape)
+    print("Testing Data ", X_test.shape)
+
+def main():
+    MarvellousRegression("Advertising.csv")
+
+if __name__ == "__main__":
+    main()
